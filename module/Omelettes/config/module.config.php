29cd378ecaead85372c6d872d964a6fb38c10f74
dave <david@omelett.es>
2013-08-12 14:47:34 +0100

    Framework for form partials

--- module/Omelettes/config/module.config.php
+++ module/Omelettes/config/module.config.php
@@ -1,3 +1,12 @@
 <?php
 
-return array();
+return array(
+	'view_manager' => array(
+		'display_not_found_reason'	=> true,
+		'display_exceptions'		=> true,
+		'doctype'					=> 'HTML5',
+		'template_path_stack' => array(
+			__DIR__ . '/../view',
+		),
+	),
+);
